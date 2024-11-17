d54bcf78e678e652ad1d14312890a4553039ac78
Daryl Stensland <dstensland5@gmail.com>
2024-11-17 14:04:56 +0000

    Text component created

---
+++ app/atomic/Atoms/Text/Text.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Text.module.css';
+
+const cn = require('classnames');
+
+const Text = ({
+  varient = 'body',
+  color = 'black',
+  weight = 'normal',
+  children,
+}) => {
+  const classname = cn(styles[varient], styles[color], styles[weight]);
+
+  switch (varient) {
+    case 'h1':
+      return <h1 className={classname}>{children}</h1>;
+    
+    case 'h2':
+      return <h2 className={classname}>{children}</h2>;
+    case 'h3':
+      return <h3 className={classname}>{children}</h3>;
+    case 'h4':
+      return <h4 className={classname}>{children}</h4>;
+    case 'body':
+      return <p className={classname}>{children}</p>;
+
+    default:
+      break;
+  }
+  return <div>Text.component</div>;
+};
+
+export default Text;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import styles from './page.module.css';
 
+import { Text } from '@/atomic';
+
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Text varient='h1'>Cyber Solution</Text>
       <p>Cyber Solutions</p>
     </div>
   );
